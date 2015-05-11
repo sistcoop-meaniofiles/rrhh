c7c3994ec92a7ad1fe0ad58e4ad33c1934989774
Carlos <carlosthe19916@gmail.com>
2015-05-11 10:30:21 -0500

    actualizar trabajador

--- public/controllers/trabajador/Rrhh.EditarTrabajador.DatosPrincipalesController.js
+++ public/controllers/trabajador/Rrhh.EditarTrabajador.DatosPrincipalesController.js
@@ -2,15 +2,17 @@
 
 /* jshint -W098 */
 angular.module('mean.rrhh').controller('Rrhh.EditarTrabajador.DatosPrincipalesController',
-    function($scope, trabajador, SGPersonaNatural, SGSucursal, SGAgencia) {
+    function($scope, trabajador, SGPersonaNatural, SGSucursal, SGAgencia, toastr) {
 
         $scope.view = {
             trabajador: trabajador
         };
 
         $scope.view.loaded = {
-            persona: SGPersonaNatural.$findByTipoNumeroDocumento($scope.view.trabajador.tipoDocumento, $scope.view.trabajador.numeroDocumento).$object,
-            agencia: SGAgencia.$find($scope.view.trabajador.agencia.id).$object
+            persona: undefined,
+            sucursal: undefined,
+            agencia: undefined,
+            id : 2
         };
 
         $scope.combo = {
@@ -27,15 +29,52 @@ angular.module('mean.rrhh').controller('Rrhh.EditarTrabajador.DatosPrincipalesCo
             $scope.$watch('combo.selected.sucursal', function(){
                 if(angular.isDefined($scope.combo.selected.sucursal)){
                     $scope.combo.agencia = $scope.combo.selected.sucursal.$getAgencias().$object;
+                    $scope.combo.selected.agencia = undefined;
                 }
             }, true);
         };
         $scope.loadCombo();
 
+        $scope.loadPersona = function(){
+            var tipoDocumento = $scope.view.trabajador.tipoDocumento;
+            var numeroDocumento = $scope.view.trabajador.numeroDocumento;
+            $scope.view.loaded.persona = SGPersonaNatural.$findByTipoNumeroDocumento(tipoDocumento, numeroDocumento).$object;
+        };
+        $scope.loadPersona();
+
+        $scope.loadAgenciaSucursal = function(){
+            var agenciaId = $scope.view.trabajador.agencia.id;
+            SGAgencia.$find(agenciaId).then(function(response){
+                $scope.view.loaded.agencia = response;
+                $scope.view.loaded.sucursal = response.sucursal;
+            });
+        };
+        $scope.loadAgenciaSucursal();
+
         $scope.submit = function(){
 
             if ($scope.form.$valid) {
-               alert('todavia no esta funcionando');
+
+                var trabajajador = {};
+                angular.extend( trabajajador,
+                    $scope.view.trabajador,
+                    {
+                        agencia: {
+                            id: $scope.combo.selected.agencia.id,
+                            codigo: $scope.combo.selected.agencia.codigo
+                        }
+                    }
+                );
+
+                trabajajador.$save().then(
+                    function(response){
+                        toastr.success('Trabajador actualizado satisfactoriamente');
+                    },
+                    function error(err){
+                        toastr.error(err.data.message);
+                    }
+                );
+
             }
 
         };
